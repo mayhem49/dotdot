89ae7a45799dfd62496b54f14f8c8732c35c6835
mayhem49 <thapamayhem49@gmail.com>
2024-07-15 18:40:50 +0545

    remove mix as compiler since not supported

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -2,7 +2,7 @@ vim.api.nvim_command([[autocmd FileType help lua vim.cmd('wincmd L')]])
 
 -- for make build
 vim.api.nvim_command([[ autocmd FileType rust compiler cargo ]])
-vim.api.nvim_command([[ autocmd FileType elixir compiler mix ]])
+--vim.api.nvim_command([[ autocmd FileType elixir compiler mix ]])
 
 --vim.api.nvim_command([[ autocmd QuickFixCmdPost [^l]* cwindow ]])
 --automatically open trouble window when a command that populates quickfix list
