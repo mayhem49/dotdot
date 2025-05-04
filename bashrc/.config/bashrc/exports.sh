3ac9b2c91c91430e9cf503ed5d07e1aa10b7b146
mayhem49 <thapamayhem49@gmail.com>
2025-05-05 00:45:26 +0545

    change config to install global npm packages locally for each user

--- bashrc/.config/bashrc/exports.sh
+++ bashrc/.config/bashrc/exports.sh
@@ -1,5 +1,10 @@
 export SUDO_EDITOR="nvim"
 
+# install global packages separately in each user's home directory
+# This avoids permission issues
+export NPM_CONFIG_PREFIX=~/.npm-global
+export PATH=$PATH:~/.npm-global/bin
+
 # I forgot for which apps this was done for
 # probably snowsql?
 #export PATH=~/bin:$PATH
