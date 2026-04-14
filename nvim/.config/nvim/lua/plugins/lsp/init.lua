9bf87c0512ac6a097efd2cef479616c4f31a55d5
mayhem49 <thapamayhem49@gmail.com>
2026-04-15 00:32:42 +0545

    lsp stuff idk

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,71 +1,19 @@
 return {
-  {
-    "neovim/nvim-lspconfig"
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      'ts_ls',
+      'eslint',
+      'html',
+      'cssls',
+      'gopls',
+      'lua_ls',
+      'expert',
+    },
+    automatic_enable = true,
   },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({})
-    end
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          'ts_ls',
-          'eslint',
-          'html',
-          'cssls',
-          'gopls',
-          'lua_ls'
-        },
-      })
-
-      vim.lsp.config('lua_ls', {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-      })
-
-      vim.lsp.config('ts_ls', {})
-
-      vim.lsp.config('tailwindcss', {
-        -- filetypes = { "html", "elixir", "eelixir", "heex" },
-        -- init_options = {
-        --   userLanguages = {
-        --     elixir = "html-eex",
-        --     eelixir = "html-eex",
-        --     heex = "html-eex",
-        --   },
-        -- },
-        filetypes = { "html", "eelixir", "heex" },
-        init_options = {
-          userLanguages = {
-            eelixir = "html-eex",
-            heex = "html-eex",
-          },
-        },
-      })
-
-      vim.lsp.config('gopls', {
-        settings = {
-          gopls = {
-            ["formatting.gofumpt"] = false,
-            --["formatting.formatTool"] = "goimports", -- optional
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-          }
-        }
-
-      })
-      --local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    end
-  }
 }
